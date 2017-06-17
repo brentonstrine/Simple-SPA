b1b5f822b6fb0ed0097e9b15a5403b9b5ef334ae
Brenton Strine <brenton.strine@gmail.com>
2017-06-17 10:06:47 -0400

    add data and ajax get it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(function() {
+    var getData = function() {
+        $.ajax({
+            url: "database/app.json",
+            dataType: "json",
+            success: function(response){
+                data = response;
+                buildSite(data);
+            },
+            error: function(response){
+                console.log("some ajax porblem.")
+            }
+        })
+    };
+    getData();
 
+    var buildSite = function(db){
+        
+    };
+});
