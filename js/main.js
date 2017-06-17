94a7968f364499c75a98b0b09c8d619d716fde54
Brenton Strine <brenton.strine@gmail.com>
2017-06-17 10:33:41 -0400

    Initial population of homepage

--- js/main.js
+++ js/main.js
@@ -15,6 +15,27 @@ $(function() {
     getData();
 
     var buildSite = function(db){
-        
+        var currentPage = "home"
+        var header = document.querySelector(".js-navbar");
+        var siteHeader = header.querySelector(".js-site__heading");
+        var content = document.querySelector(".js-content");
+        var pageHeading = content.querySelector(".js-page__heading");
+        var pageContent = content.querySelector(".js-page__content");
+        var footer = document.querySelector(".js-footer");
+
+        //populate header
+        pageHeading.textContent = db.pages[currentPage].heading;
+
+        //populate main content
+        var mainFrag = "";
+        db.pages[currentPage].main.forEach(function(paragraph){
+            mainFrag += "<p>" + paragraph + "</p>";
+        });
+        pageContent.innerHTML = mainFrag;
+
+        //populate footer
+        var currentYear = "2017";
+        var footerString = db.components.footer.replace("{year}", currentYear);
+        footer.innerHTML ="<p>" + footerString + "</p>";
     };
 });
